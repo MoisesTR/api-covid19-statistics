e6b07139ce07ec8e00d0d384bf50585611045cc6
mtrigueros <moisestrigueros@hotmail.com>
2021-01-31 16:02:51 -0600

    SC-1 Establish base structure of the API
    1- Configure husky, pre-commit stage
    2- Configure pretty-quick to run prettier in the staged files
    3- Add file configuration for .prettierignore and .prettierrc

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    semi: true,
+    tabWidth: 4,
+    printWidth: 100,
+    singleQuote: true,
+    endOfLine: 'auto',
+    trailingComma: 'all',
+    arrowParens: 'always',
+};
